140e34d2ee89ad192ef299a9716be6be566e93b9
@Jorge_Kleber <jorge_kleber@outlook.com>
2021-04-06 19:45:36 -0400

    Reformulado a interação do Repository com a api

--- app/src/main/java/com/dev/kleber/github/search/repository/search/impl/remote/SearchRemoteRepositoryImpl.kt
+++ app/src/main/java/com/dev/kleber/github/search/repository/search/impl/remote/SearchRemoteRepositoryImpl.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import com.dev.kleber.github.BuildConfig
 import com.dev.kleber.github.search.data.SearchResult
 import com.dev.kleber.github.search.network.SearchRepositoryAPI
+import com.dev.kleber.github.search.network.SearchRepositoryInfrastructure
 import com.dev.kleber.github.search.repository.search.SearchRepository
 import retrofit2.Call
 import retrofit2.Callback
@@ -11,30 +12,11 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class SearchRemoteRepositoryImpl : SearchRepository, Callback<SearchResult> {
+class SearchRemoteRepositoryImpl : SearchRepository {
 
     override fun searchRepo() {
 
-        val retrofit = Retrofit.Builder()
-            .baseUrl(BuildConfig.SERVER_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-        val service : SearchRepositoryAPI = retrofit.create(SearchRepositoryAPI::class.java)
-        service.searchRepositories().enqueue(this)
+        SearchRepositoryInfrastructure().invoke()
 
     }
-
-    override fun onResponse(call: Call<SearchResult>, response: Response<SearchResult>) {
-
-        Log.d("TESTE","VICTOR")
-
-    }
-
-    override fun onFailure(call: Call<SearchResult>, t: Throwable) {
-
-        Log.d("TESTE","VICTOR")
-    }
-
-
 }
